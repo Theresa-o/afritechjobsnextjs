ef616db0c41735c1662546b19bc9de8c79de428d
theresa-o <okorotheresa19@gmail.com>
2025-03-22 09:13:54 +0100

    Updated backend URL for fetching jobs

--- src/app/(routes)/jobs/[id]/page.tsx
+++ src/app/(routes)/jobs/[id]/page.tsx
@@ -8,24 +8,14 @@ import axios from "axios";
 
 const JobListing = async ({ params }: { params: { id: string } }) => {
   const queryClient = new QueryClient();
-  // console.log(params.id);
-  // console.log("hiiii");
-  // const { id } = params.id
-  const id = Number(params.id);
 
-  // async function getJob(id: number) {
-  //   try {
-  //     const res = await fetch(`http://localhost:3000/jobs/${id}/`);
-  //     console.log(res);
-  //     return await res.json();
-  //   } catch (err: any) {
-  //     throw err.response.data;
-  //   }
-  // }
+  const id = Number(params.id);
 
   async function getJob(id: number) {
     try {
-      const res = await axios.get(`http://127.0.0.1:8000/jobs/${id}/`);
+      const res = await axios.get(
+        `https://teresita.pythonanywhere.com/jobs/${id}/`
+      );
       return await res.data;
     } catch (err: any) {
       console.error("Failed to fetch job:", err);
